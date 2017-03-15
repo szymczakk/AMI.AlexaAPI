645428102b061ba11528123e4332a0f7b31cc7aa
szymczakk <rakaz666@gmail.com>
2017-03-15 17:30:28 +0100

    fix trafis-build again

--- travis-build.sh
+++ travis-build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
-dotnet restore ./AMI.AlexaAPI/
-dotnet restore ./AMI.AlexaAPI.Tests/
-dotnet build **/*.csproj
+dotnet restore ./AMI.AlexaAPI/AMI.AlexaAPI.csproj
+dotnet restore ./AMI.AlexaAPI.Tests/AMI.AlexaAPI.Tests.csproj
+dotnet build ./AMI.AlexaAPI/AMI.AlexaAPI.csproj
+dotnet build ./AMI.AlexaAPI.Tests/AMI.AlexaAPI.Tests.csproj
