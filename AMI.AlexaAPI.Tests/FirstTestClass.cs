16bceda44ca402cfec19ffe8dca8adf2599608e4
szymczakk <rakaz666@gmail.com>
2017-03-15 17:24:13 +0100

    add test project

---
+++ AMI.AlexaAPI.Tests/FirstTestClass.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace AMI.AlexaAPI.Tests
+{
+    public class FirstTestClass
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
